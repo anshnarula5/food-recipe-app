e4084f24032401629622e753bb15987889e8c0fa
Ansh Narula <2019210@iiitdmj.ac.in>
2022-03-05 02:01:12 +0530

    nutrition

--- screens/Home/Recipe.jsx
+++ screens/Home/Recipe.jsx
@@ -1,18 +1,30 @@
 import { Text, Layout } from "@ui-kitten/components";
 import React, { useEffect, useRef } from "react";
-import { Animated, View, StyleSheet, Image } from "react-native";
+import { Animated, View, StyleSheet, Image, ScrollView } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { SharedElement } from "react-navigation-shared-element";
 import Icon from "react-native-vector-icons/AntDesign";
 import { SimpleLineIcons } from "react-native-vector-icons";
 import { FontAwesome } from "react-native-vector-icons";
-import { capitalize, pBlue, pGreen, pPink, pPurple, pSky } from "../../constants";
+import * as Progress from "react-native-progress";
+import {
+  capitalize,
+  pBlue,
+  pGreen,
+  pPink,
+  pPurple,
+  pSky,
+  pViolet,
+} from "../../constants";
 
 const Recipe = ({ route, navigation }) => {
   const { recipe } = route.params;
+  const nuts = Object.values(recipe.totalDaily);
+  const filteredNuts = nuts.slice(0, 7);
   const safeInsets = useSafeAreaInsets();
   const opacity = useRef(new Animated.Value(0)).current;
   useEffect(() => {
+    console.log(nuts);
     Animated.timing(opacity, {
       toValue: 1,
       duration: 200,
@@ -22,7 +34,7 @@ const Recipe = ({ route, navigation }) => {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Animated.View
         style={{
           opacity,
@@ -58,27 +70,46 @@ const Recipe = ({ route, navigation }) => {
         <Layout style={styles.cards}>
           <Layout style={{ backgroundColor: pPurple, ...styles.card }}>
             <Icon name="clockcircleo" size={30} />
-            <View style= {{alignItems : "center", paddingTop : 7}}>
-              <Text>{5 + Math.floor(Math.random()*30)} </Text>
+            <View style={{ alignItems: "center", paddingTop: 7 }}>
+              <Text>{5 + Math.floor(Math.random() * 30)} </Text>
               <Text>min</Text>
             </View>
           </Layout>
           <Layout style={{ backgroundColor: pSky, ...styles.card }}>
             <SimpleLineIcons name="fire" size={30} />
-            <View style= {{alignItems : "center", paddingTop : 7}}>
-            <Text >{recipe.calories.toFixed(2)}</Text>
+            <View style={{ alignItems: "center", paddingTop: 7 }}>
+              <Text>{recipe.calories.toFixed(2)}</Text>
 
               <Text>Calories</Text>
             </View>
           </Layout>
           <Layout style={{ backgroundColor: pPink, ...styles.card }}>
-           <FontAwesome name="heartbeat" size={30} />
-           
+            <FontAwesome name="heartbeat" size={30} />
             <Text style={styles.text}>{recipe.healthLabels[0]}</Text>
           </Layout>
         </Layout>
+        <Text
+          category={"h3"}
+          style={{ fontWeight: "200", paddingVertical: 10 }}
+        >
+          Nutrition
+        </Text>
+        <Layout>
+          {filteredNuts.map((f) => (
+            <Layout>
+              <Text>{f.label}</Text>
+              <Progress.Bar
+                progress={Math.random()}
+                width={300}
+                borderWidth={0.6}
+                height={10}
+                animated={false}
+              />
+            </Layout>
+          ))}
+        </Layout>
       </Layout>
-    </View>
+    </ScrollView>
   );
 };
 
